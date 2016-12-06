3169dfc9aeb8b4b758cd8b839e0fda81ef4cf93b
ShellvanDell <ShellvanDell@gmx.de>
2016-12-06 15:14:25 +0100

    WAAAHHHHH

--- sysload.m
+++ sysload.m
@@ -1,7 +1,6 @@
 function path = sysload(a,x)
 y = str2num(x);
 y=y+1;
-
 #warum = "#driver/burstgenerator/SFT400#driver/surgeg#enerator/CWG500#driver/coupler/CWG520";
 strings = strsplit(a,"#");
 string = strings(y);
